7ecb5226cda6cb7c85c4aff76da1a30b58c17e6f
hejianxian <531601727@qq.com>
2020-09-07 12:11:07 +0800

    chore: optimize docs

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   title: 'WorkPlusFE 开发手册',
-  base: '/w6s-cli',
+  base: '/w6s-cli/',
   head: [
     ['link', { rel: 'icon', href: '/favicon.ico' }]
   ],
@@ -12,8 +12,8 @@ module.exports = {
       { text: '常用模块', link: '/packages' },
       { text: '常见问题', link: '/qa' },
       { text: '安卓真机调试', link: '/debug' },
-      { text: 'H5 SDK', link: 'https://github.com/WorkPlusFE/sdk.js' },
-      { text: 'Cordova 文档', link: 'https://workplusfe.github.io/cordova-docs/' },
+      { text: '开放平台', link: 'https://open.workplus.io/v4/' },
+      { text: 'Cordova 文档', link: 'https://open.workplus.io/cordova/' },
     ],
     lastUpdated: '最后更新时间',
     repo: 'workplusfe/w6s-cli',
