1f0249fb91c90a6035cb1fe6fd31f58a67a15758
hejianxian <531601727@qq.com>
2020-09-17 23:14:19 +0800

    chore(cli-script): fix process env

--- packages/@w6s/cli-script/lib/index.js
+++ packages/@w6s/cli-script/lib/index.js
@@ -13,28 +13,14 @@ const stylelintPlugin = toPlugin('@w6s/stylelint-plugin');
 const i18nPlugin = toPlugin('vue-cli-plugin-i18n');
 
 const context = process.cwd();
-let hasW6sConfigFile = false;
 
 // w6s.config.js
 const configPath = path.resolve(context, 'w6s.config.js');
 if (configPath && w6sConfigFileExists(configPath)) {
   process.env.VUE_CLI_SERVICE_CONFIG_PATH = configPath;
-  hasW6sConfigFile = true;
 }
 
 const createService = (command) => {
-  let inlineOptions = {};
-
-  if (hasW6sConfigFile) {
-    // 跟vue-cli保持一致，build 与 test 命令默认为 production 的 node 环境，其他均为 development
-    if (command.toLowerCase() === 'build' || command.toLowerCase() === 'test') {
-      process.env.NODE_ENV = 'production';
-    } else {
-      process.env.NODE_ENV = 'development';
-    }
-    inlineOptions = require(configPath);
-  }
-  
   const projectPlugins = getRootProjectPlugins(context) || [];
   const inlinePlugins = [
     babelPlugin,
@@ -55,7 +41,6 @@ const createService = (command) => {
       lintOnSave: 'default',
     },
     inlineOptions: {
-      ...inlineOptions,
       parallel: false, // disable thread-loader
     },
     plugins: inlinePlugins,
